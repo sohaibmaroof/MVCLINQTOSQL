55ada6253b064f9668e9c878be2c6ad874dfc59d
unknown <Sohaib.Maroof@tombola-0274.tombola.emea>
2015-09-15 17:09:05 +0100

    creating less file by copying css from bootstrap.css

--- MVCLINQTOSQL/App_Start/BundleConfig.cs
+++ MVCLINQTOSQL/App_Start/BundleConfig.cs
@@ -23,9 +23,9 @@ namespace MVCLINQTOSQL
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+           // bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/StyleSheet1.css"));
+           bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/StyleSheet1.css"));
+
         }
     }
 }
