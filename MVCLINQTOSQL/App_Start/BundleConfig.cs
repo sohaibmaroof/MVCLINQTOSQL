ae589c0c4ce567ce272d72507531fba7adacfbe3
unknown <Sohaib.Maroof@tombola-0274.tombola.emea>
2015-09-16 16:12:18 +0100

    copied css code from bootstrap into site.less file

--- MVCLINQTOSQL/App_Start/BundleConfig.cs
+++ MVCLINQTOSQL/App_Start/BundleConfig.cs
@@ -24,8 +24,10 @@ namespace MVCLINQTOSQL
                       "~/Scripts/respond.js"));
 
            // bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/StyleSheet1.css"));
-           // bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/StyleSheet1.css"));
-       bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+        // bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+
+        // bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/StyleSheet1.css"));
+      bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
         }
     }
